40d14c39c60aca4a296f63d465c506ca527553e6
sonia250 <s.uwase3@alustudent.com>
2024-11-07 14:03:55 +0200

    ch:initializing the db

--- database.py
+++ database.py
@@ -5,3 +5,13 @@ def create_connection():
     conn = sqlite3.connect("reminder_management.db")
     return conn
 
+#initializing the database
+def initialize_db():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS reminders (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        description TEXT,
+                        due_date TEXT)''')
+    conn.commit()
+    conn.close()
