d4bf01f3a62ef26a71e224acfa37126273c3aef6
65laura <ishimwelauraceline@gmail.com>
2024-11-14 13:09:37 +0200

    ft:database setup

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 import sqlite3
 
-# Connect to the SQLite database
-def create_connection():
-    conn = sqlite3.connect("reminder_management.db")
-    return conn
+class Database:
+    def __init__(self, db_name="reminder_management.db"):
+        self.db_name = db_name
+
+    # Connect to the SQLite database
+    def create_connection(self):
+        return sqlite3.connect(self.db_name)
